b3709a26de97cbe43c2113ac29d74b73a109b5dd
Lukacarlsen <lukacarlsen7@gmail.com>
2026-03-08 17:06:58 -1200

    Add files via upload

---
+++ fer.js
@@ -0,0 +1,6 @@
+const boton = document.getElementById('botonMafer');
+const mensaje = document.getElementById('mensaje');
+boton.addEventListener('click', () => {
+    mensaje.style.opacity = "1";
+});
+alert ("hey querida mafer a donde vas eh, bueno sigue esto fue echo con mucho cariño, te quiero ")
